976d3fcf44092b5a77674508c1be8796783dc0e8
Jeffrey <jeffrey.liu90@gmail.com>
2020-02-12 00:48:41 +0000

    the survey for your personal profile

--- survey.js
+++ survey.js
@@ -6,51 +6,54 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What is your name? Nicknames are also acceptable :) ', (answer) => {
-  // TODO: Log the answer in a database
-  console.log(`Thank you for your valuable feedback:: ${answer}`);
+let survey = {}
 
-  
+rl.question('What is your name? Nicknames are also acceptable :) ', (answer) => {
+  survey["name"] = answer;
+
+
   rl.question('What is an activity you like doing? ', (answer) => {
     // TODO: Log the answer in a database
-    console.log(`Thank you for your valuable feedback: ${answer}`);
+    survey["activity"] = answer;
 
 
-  rl.question('What do you listen to while doing that? ', (answer) => {
-    // TODO: Log the answer in a database
-    console.log(`Thank you for your valuable feedback: ${answer}`);
-
-
-  rl.question('Whhich meal is your favourite? (eg: dinner, brunch, etc.) ', (answer) => {
-    // TODO: Log the answer in a database
-    console.log(`Thank you for your valuable feedback: ${answer}`);
-
-
-
-  rl.question('What\'s your favourite thing to eat for that meal?', (answer) => {
-    // TODO: Log the answer in a database
-    console.log(`Thank you for your valuable feedback: ${answer}`);
-
-
-  rl.question('Which sport is your absolute favourite?', (answer) => {
-    // TODO: Log the answer in a database
-    console.log(`Thank you for your valuable feedback: ${answer}`);
-
-
-  
-    rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (answer) => {
+    rl.question('What do you listen to while doing that? ', (answer) => {
       // TODO: Log the answer in a database
-      console.log(`Thank you for your valuable feedback: ${answer}`);
+      survey["music"] = answer;
+
+
+      rl.question('Whhich meal is your favourite? (eg: dinner, brunch, etc.) ', (answer) => {
+        // TODO: Log the answer in a database
+        survey["meal"] = answer;
 
 
 
+        rl.question('What\'s your favourite thing to eat for that meal?', (answer) => {
+          // TODO: Log the answer in a database
+          survey["food"] = answer;
 
 
-  rl.close();
-});
-});
-});
-});
-});
-});
-});
+          rl.question('Which sport is your absolute favourite?', (answer) => {
+            // TODO: Log the answer in a database
+            survey["sport"] = answer;
+
+
+
+            rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (answer) => {
+              // TODO: Log the answer in a database
+              survey["superpower"] = answer;
+
+
+              console.log(`Thank you ${survey.name}, I see that you like ${survey.activity}, while listening to ${survey.music}. Your favourite meal is ${survey.meal} and your favourite food is ${survey.food}. Your favourite sport is ${survey.sport} and your superpower is ${survey.superpower}. Ciao!`)
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+})
+
+
+ 
+
